17a1b3e0015db3313efd81454fc3e9ffde7c08a1
Femaref <femaref@googlemail.com>
2017-07-06 14:23:36 +0200

    init

---
+++ reliable_conn.go
@@ -0,0 +1,127 @@
+package reliable_conn
+
+import (
+	"fmt"
+	"net"
+	"sync"
+	"time"
+)
+
+type ReliableConn struct {
+	Network string
+	Address string
+
+	isConnected    bool
+	isReconnecting bool
+	m              *sync.Mutex
+	internal       net.Conn
+	q_m            *sync.Mutex
+	queue          [][]byte
+	dialer         Dialer
+}
+
+
+type Dialer func(network, address string) (net.Conn, error)
+
+func Dial(network, address string) (net.Conn, error) {
+    return DialWithDialer(network, address, nil)
+}
+
+func DialWithDialer(network, address string, dialer Dialer) (net.Conn, error) {
+    if dialer == nil {
+        dialer = net.Dial
+    }
+	return &ReliableConn{
+	    Network: network,
+	    Address: address,
+	    m: new(sync.Mutex),
+	    q_m: new(sync.Mutex),
+	    dialer: dialer,
+	}, nil
+}
+
+func (this *ReliableConn) Connect() {
+	var cp []byte
+	this.m.Lock()
+	defer this.m.Unlock()
+
+	if this.isConnected || this.isReconnecting {
+		return
+	}
+	this.isReconnecting = true
+	this.isConnected = false
+
+	var local_error error = fmt.Errorf("err")
+	var new_conn net.Conn
+	for local_error != nil {
+		new_conn, local_error = this.dialer(this.Network, this.Address)
+		fmt.Println(local_error)
+		time.Sleep(time.Second)
+	}
+	this.internal = new_conn
+	fmt.Println("reconnected")
+	this.isConnected = true
+	this.isReconnecting = false
+
+	this.q_m.Lock()
+	defer this.q_m.Unlock()
+	for _, elem := range this.queue {
+		cp = make([]byte, len(elem))
+		copy(cp, elem)
+		go this.Write(cp)
+	}
+	this.queue = [][]byte{}
+
+}
+
+func (this *ReliableConn) Read(b []byte) (n int, err error) {
+	if !this.isConnected {
+		this.Connect()
+	}
+	return this.internal.Read(b)
+}
+func (this *ReliableConn) Write(b []byte) (n int, err error) {
+	var orig_err error
+	cp := make([]byte, len(b))
+	copy(cp, b)
+	if this.isConnected {
+		n, orig_err = this.internal.Write(cp)
+	}
+	if !this.isConnected || orig_err != nil {
+		fmt.Println("disconnected")
+		this.q_m.Lock()
+		this.queue = append(this.queue, cp)
+		this.q_m.Unlock()
+		this.isConnected = false
+
+		go this.Connect()
+
+		return len(b), nil
+	}
+
+	return
+}
+
+func (this *ReliableConn) Close() error {
+	return this.internal.Close()
+}
+
+func (this *ReliableConn) LocalAddr() net.Addr {
+	return this.internal.LocalAddr()
+}
+
+func (this *ReliableConn) RemoteAddr() net.Addr {
+	return this.internal.RemoteAddr()
+}
+
+func (this *ReliableConn) SetDeadline(t time.Time) error {
+	return this.internal.SetDeadline(t)
+}
+
+func (this *ReliableConn) SetReadDeadline(t time.Time) error {
+	return this.internal.SetReadDeadline(t)
+}
+
+func (this *ReliableConn) SetWriteDeadline(t time.Time) error {
+	return this.internal.SetWriteDeadline(t)
+}
---
+++ reliable_conn_test.go
@@ -0,0 +1,265 @@
+package reliable_conn
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+	"crypto/tls"
+)
+
+type Listener func(network, laddr string) (net.Listener, error)
+
+func listenerForTest(port string, expected_total_conns int) (string, net.Listener, func(string) bool, error) {
+    return listenerForTestWithListener(port, expected_total_conns, nil)
+}
+
+func listenerForTestWithListener(port string, expected_total_conns int, l Listener) (string, net.Listener, func(string) bool, error) {
+    if l == nil {
+        l = net.Listen
+    }
+	host := fmt.Sprintf("127.0.0.1:%s", port)
+	listener, err := l("tcp", host)
+	if err != nil {
+		return "", nil, nil, err
+	}
+
+	_, port, err = net.SplitHostPort(listener.Addr().String())
+
+	if err != nil {
+		listener.Close()
+		return "", nil, nil, err
+	}
+
+	m := new(sync.Mutex)
+	wait_for := make(map[string]bool)
+	conns := 0
+	go func() {
+		for {
+			// Wait for a connection.
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+
+			go func(c net.Conn) {
+				defer c.Close()
+				m.Lock()
+				conns++
+				if conns > expected_total_conns {
+					fmt.Println("more than expected conns")
+					listener.Close()
+					return
+				}
+				m.Unlock()
+
+				defer func() {
+					m.Lock()
+					conns--
+					m.Unlock()
+				}()
+				for {
+					var err error
+					var buffer []byte
+					buffer, err = waitFor(c)
+					if err != nil {
+						return
+					}
+					s := string(buffer)
+					results := strings.Split(s, "\n")
+					m.Lock()
+					for _, elem := range results {
+						wait_for[elem] = true
+					}
+					m.Unlock()
+
+					_, err = c.Write(buffer)
+					if err != nil {
+						return
+					}
+				}
+			}(conn)
+		}
+	}()
+
+	return port, listener, func(s string) bool { val, ok := wait_for[s]; return ok && val }, nil
+}
+
+func waitFor(conn net.Conn) (buffer []byte, err error) {
+	var bytes_read int
+
+	buffer = make([]byte, 1024)
+	for bytes_read == 0 {
+		bytes_read, err = conn.Read(buffer)
+		if err != nil {
+			return nil, err
+		}
+
+		time.Sleep(250 * time.Millisecond)
+	}
+
+	return buffer[:bytes_read], nil
+}
+
+func TestListener(t *testing.T) {
+	var err error
+	port, listener, did_receive, err := listenerForTest("0", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+	host := fmt.Sprintf("127.0.0.1:%s", port)
+	conn, err := net.Dial("tcp", host)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	to_send := "ping"
+	_, err = fmt.Fprintf(conn, to_send)
+	if err != nil {
+		t.Fatal(err)
+	}
+	buffer, err := waitFor(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if to_send != string(buffer) {
+		t.Fatal("buffer not equal to to_send")
+	}
+
+	if !did_receive(to_send) {
+		t.Fatal("listener did not receive")
+	}
+}
+
+func TestNormalOperation(t *testing.T) {
+	var err error
+	port, listener, _, err := listenerForTest("0", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+	host := fmt.Sprintf("127.0.0.1:%s", port)
+	conn, err := Dial("tcp", host)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	to_send := "ping"
+	_, err = fmt.Fprintf(conn, to_send)
+	if err != nil {
+		t.Fatal(err)
+	}
+	buffer, err := waitFor(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if to_send != string(buffer) {
+		t.Fatal("buffer not equal to to_send")
+	}
+}
+
+func TestMonkeyRead(t *testing.T) {
+	var err error
+	port, listener, _, err := listenerForTest("0", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+	host := fmt.Sprintf("127.0.0.1:%s", port)
+	conn, err := Dial("tcp", host)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	rc := conn.(*ReliableConn)
+	rc.Connect()
+
+	rc.internal.Close()
+	buffer := make([]byte, 1024)
+	_, err = rc.Read(buffer)
+}
+
+func TestMonkeyWrite(t *testing.T) {
+	var err error
+	port, listener, did_receive, err := listenerForTest("0", 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+	host := fmt.Sprintf("127.0.0.1:%s", port)
+	conn, err := Dial("tcp", host)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	rc := conn.(*ReliableConn)
+	rc.Connect()
+	fmt.Fprintf(conn, "before_close")
+	rc.internal.Close()
+
+	time.Sleep(1000 * time.Millisecond)
+
+	to_send := []string{"ping", "pong", "pow"}
+	for _, elem := range to_send {
+		fmt.Fprintf(conn, fmt.Sprintf("%s\n", elem))
+	}
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(2000 * time.Millisecond)
+
+	for _, elem := range to_send {
+		if !did_receive(elem) {
+			t.Errorf("listener did not receive %s", elem)
+		}
+	}
+}
+
+func TestTLS(t *testing.T) {
+    cer, err := tls.LoadX509KeyPair("testdata/server.crt", "testdata/server.key")
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    config := &tls.Config{Certificates: []tls.Certificate{cer}}
+
+    l := func(network, laddr string) (net.Listener, error) {
+        return tls.Listen(network, laddr, config)
+    }
+
+    d := func(network, address string) (net.Conn, error) {
+        config := &tls.Config{InsecureSkipVerify:true}
+        return tls.Dial(network, address, config)
+    }
+
+    port, listener, _, err := listenerForTestWithListener("0", 1, l)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+	host := fmt.Sprintf("127.0.0.1:%s", port)
+	conn, err := DialWithDialer("tcp", host, d)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	to_send := "ping"
+	_, err = fmt.Fprintf(conn, to_send)
+	if err != nil {
+		t.Fatal(err)
+	}
+	buffer, err := waitFor(conn)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if to_send != string(buffer) {
+		t.Fatal("buffer not equal to to_send")
+	}
+}
